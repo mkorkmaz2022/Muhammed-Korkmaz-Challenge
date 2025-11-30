20a933b516beeffcd2acc704c4281dbb17379a23
muhammedkorkmaz1 <151931061+muhammedkorkmaz1@users.noreply.github.com>
2025-11-30 15:19:32 +0300

    Fix: Pages deploy settings

--- ui/src/utility/arena/battle.ts
+++ ui/src/utility/arena/battle.ts
@@ -1,7 +1,6 @@
 import { Transaction } from "@mysten/sui/transactions";
 
 export const battle = (packageId: string, heroId: string, arenaId: string) => {
-  const tx = new Transaction();
 
   // TODO: Add moveCall to start a battle
   // Function: `${packageId}::arena::battle`
@@ -10,6 +9,7 @@ export const battle = (packageId: string, heroId: string, arenaId: string) => {
   // Use tx.object() for both hero and battle place objects
   // The battle winner is determined by hero power comparison
   // Winner takes both heroes
+  const tx = new Transaction();
   tx.moveCall({
     target: `${packageId}::arena::battle`,
     arguments: [tx.object(heroId), tx.object(arenaId)]
